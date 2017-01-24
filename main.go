1a53deaba2553d7d9146600bf5be08a895f91491
DinoLai <dinos80152@gmail.com>
2017-01-25 00:48:09 +0800

    make a hello world web service, close #11

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
 	"github.com/liam-lai/ptt-alertor/hello"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r.RemoteAddr + " visit: " + r.RequestURI)
+    fmt.Fprintf(w, hello.HelloWorld())
+}
+
 func main() {
-	fmt.Print(hello.HelloWorld())
+    http.HandleFunc("/", index)
+    err := http.ListenAndServe(":9090", nil)
+    if err != nil {
+        log.Fatal("ListenAndServer ", err)
+    }
 }
