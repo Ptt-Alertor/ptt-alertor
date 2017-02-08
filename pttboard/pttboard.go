106140934aa18d6d554aa405931174c62d030899
DinoLai <dinos80152@gmail.com>
2017-02-09 01:41:11 +0800

    parse article title and link

--- pttboard/pttboard.go
+++ pttboard/pttboard.go
@@ -17,6 +17,8 @@ type article struct {
 	author string
 }
 
+var articles []article
+
 func fetchHTML(board string) (response *http.Response) {
 	response, err := http.Get("https://www.ptt.cc/bbs/" + board + "/index.html")
 
@@ -27,57 +29,98 @@ func fetchHTML(board string) (response *http.Response) {
 	return response
 }
 
-func parseHTMLByToken(response *http.Response) {
-	tokenizer := html.NewTokenizer(response.Body)
-
-	for {
-		tokenType := tokenizer.Next()
-		if tokenType == html.ErrorToken {
-			return
-		}
-		if tokenType == html.StartTagToken {
-			token := tokenizer.Token()
-			if token.Data == "div" {
-				for _, tagAttr := range token.Attr {
-					if tagAttr.Key == "class" && tagAttr.Val == "title" {
-
-					}
-				}
-			}
-		}
-	}
-}
-
 func parseHTML(response *http.Response) {
 	doc, err := html.Parse(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	findTitleDiv(doc)
+	nodes := traverseHTMLNode(doc, findArticleBlocks)
+	targetNodes = make([]*html.Node, 0)
+	articles = make([]article, 0)
+	var article article
 
-}
+	for _, node := range nodes {
+		divs := traverseHTMLNode(node, findTitleDiv)
+		fmt.Println(divs)
+		for _, div := range divs {
+			targetNodes = make([]*html.Node, 0)
 
-func findTitleDiv(node *html.Node) {
-	if node.Type == html.ElementNode && node.Data == "div" {
-		for _, tagAttr := range node.Attr {
-			if tagAttr.Key == "class" && tagAttr.Val == "title" {
-				findTitleAnchor(node)
+			for _, anchor := range traverseHTMLNode(div, findAnchor) {
+				article.title = anchor.FirstChild.Data
+				article.href = getAnchorLink(anchor)
+				articles = append(articles, article)
 			}
 		}
 	}
-	for child := node.FirstChild; child != nil; child = child.NextSibling {
-		findTitleDiv(child)
+
+	for _, article := range articles {
+		fmt.Println(article)
 	}
 }
 
-func findTitleAnchor(node *html.Node) {
-	if node.Type == html.ElementNode && node.Data == "a" {
-		fmt.Println(node.FirstChild.Data)
+func getAnchorLink(anchor *html.Node) string {
+	for _, value := range anchor.Attr {
+		if value.Key == "href" {
+			return value.Val
+		}
 	}
+	return ""
+}
+
+type findInHTML func(node *html.Node) *html.Node
+
+var targetNodes []*html.Node
+
+func traverseHTMLNode(node *html.Node, find findInHTML) []*html.Node {
+
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
-		findTitleAnchor(child)
+		targetNode := find(child)
+		if targetNode != nil {
+			targetNodes = append(targetNodes, targetNode)
+
+		}
+		traverseHTMLNode(child, find)
 	}
+	return targetNodes
+}
+
+func findArticleBlocks(node *html.Node) *html.Node {
+	return findDivByClassName(node, "r-ent")
+}
+
+func findTitleDiv(node *html.Node) *html.Node {
+	return findDivByClassName(node, "title")
+}
+
+func findAnchor(node *html.Node) *html.Node {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		return node
+	}
+	return nil
+}
+
+func findMetaDiv(node *html.Node) *html.Node {
+	return findDivByClassName(node, "meta")
+}
+
+func findDateDiv(node *html.Node) *html.Node {
+	return findDivByClassName(node, "date")
+}
+
+func findAuthorDiv(node *html.Node) *html.Node {
+	return findDivByClassName(node, "author")
+}
+
+func findDivByClassName(node *html.Node, className string) *html.Node {
+	if node.Type == html.ElementNode && node.Data == "div" {
+		for _, tagAttr := range node.Attr {
+			if tagAttr.Key == "class" && tagAttr.Val == className {
+				return node
+			}
+		}
+	}
+	return nil
 }
 
 func Json() []byte {
