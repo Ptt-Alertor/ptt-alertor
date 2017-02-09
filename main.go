384be993b5b2c902cd690e70c63d1981a44ac300
DinoLai <dinos80152@gmail.com>
2017-02-09 17:08:41 +0800

    use httprouter package to refactor url to rest api

--- main.go
+++ main.go
@@ -4,28 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 	"strings"
+
+	"github.com/julienschmidt/httprouter"
 
 	"github.com/liam-lai/ptt-alertor/hello"
 	"github.com/liam-lai/ptt-alertor/pttboard"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.RemoteAddr + " visit: " + r.RequestURI)
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Println(r.RemoteAddr + " visit: " + r.URL.Path)
 	fmt.Fprintf(w, hello.HelloWorld())
 }
 
-func board(w http.ResponseWriter, r *http.Request) {
-	board := strings.ToUpper(r.FormValue("board"))
+func board(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	board := strings.ToUpper(params.ByName("boardName"))
 	fmt.Fprintf(w, "%s", pttboard.FirstPage(board))
 }
 
 func main() {
 	fmt.Println("----Web Server Start on Port 9090----")
-	http.HandleFunc("/", index)
-	http.HandleFunc("/articles", board)
-	err := http.ListenAndServe(":9090", nil)
+
+	router := httprouter.New()
+	router.GET("/", index)
+	router.GET("/board/:boardName/articles", board)
+
+	err := http.ListenAndServe(":9090", router)
 	if err != nil {
 		log.Fatal("ListenAndServer ", err)
 	}
