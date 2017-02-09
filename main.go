3758ba5162c8665d697bb03ea0d5089d00bf3b00
DinoLai <dinos80152@gmail.com>
2017-02-09 16:41:04 +0800

    get board by url query string

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 
+	"strings"
+
 	"github.com/liam-lai/ptt-alertor/hello"
 	"github.com/liam-lai/ptt-alertor/pttboard"
 )
@@ -15,12 +17,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func board(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", pttboard.FirstPage("FREE_BOX"))
+	board := strings.ToUpper(r.FormValue("board"))
+	fmt.Fprintf(w, "%s", pttboard.FirstPage(board))
 }
 
 func main() {
+	fmt.Println("----Web Server Start on Port 9090----")
 	http.HandleFunc("/", index)
-	http.HandleFunc("/board", board)
+	http.HandleFunc("/articles", board)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServer ", err)
