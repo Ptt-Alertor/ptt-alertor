266c1d986eebfeceda6605cbf688f37b9323b6ad
DinoLai <dinos80152@gmail.com>
2017-04-15 19:53:48 +0800

    implement session package, storage by memory

---
+++ session/example/example.go
@@ -0,0 +1,54 @@
+package example
+
+import (
+	"html/template"
+	"net/http"
+	"time"
+
+	_ "github.com/liam-lai/ptt-alertor/session/memory"
+	"encoding/gob"
+)
+
+var globalSessions *Manager
+
+func init() {
+	globalSessions, _ = NewManager("memory", "gosessionid", 3600)
+	go globalSessions.GC()
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	r.ParseForm()
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		w.Header().Set("Content-Type", "text/html")
+		t.Execute(w, sess.Get("username"))
+	} else {
+		sess.Set("username", r.Form["username"])
+		http.Redirect(w, r, "/", 302)
+	}
+}
+
+func count(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	createtime := sess.Get("createtime")
+	if createtime == nil {
+		sess.Set("createtime", time.Now().Unix())
+	} else if (createtime.(int64) + 360) < (time.Now().Unix()) {
+		globalSessions.SessionDestroy(w, r)
+		sess = globalSessions.SessionStart(w, r)
+	}
+	ct := sess.Get("countnum")
+	if ct == nil {
+		sess.Set("countnum", 1)
+	} else {
+		sess.Set("countnum", (ct.(int) + 1))
+	}
+	t, _ := template.ParseFiles("count.gtpl")
+	w.Header().Set("Content-Type", "text/html")
+	t.Execute(w, sess.Get("countnum"))
+}
+
+func test (){
+	gob.Register()
+}
