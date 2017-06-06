6eedc02a3cd5dbed40278751efba9b64025cd1ea
DinoLai <dinos80152@gmail.com>
2017-06-07 04:52:29 +0800

    mark html template parse for waiting dockerfile fix

--- controllers/index.go
+++ controllers/index.go
@@ -1,16 +1,18 @@
 package controllers
 
 import (
-	"html/template"
+	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/liam-lai/ptt-alertor/hello"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	t, err := template.ParseFiles("public/index.html")
-	if err != nil {
-		panic(err)
-	}
-	t.Execute(w, nil)
+	// t, err := template.ParseFiles("public/index.html")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// t.Execute(w, nil)
+	fmt.Fprintf(w, hello.HelloWorld())
 }
