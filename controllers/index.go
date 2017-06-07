b455ae5a9daa7b0800675da40b5a41526c8713ce
DinoLai <dinos80152@gmail.com>
2017-06-07 17:08:13 +0800

    enable index.html route

--- controllers/index.go
+++ controllers/index.go
@@ -1,18 +1,16 @@
 package controllers
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/liam-lai/ptt-alertor/hello"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	// t, err := template.ParseFiles("public/index.html")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// t.Execute(w, nil)
-	fmt.Fprintf(w, hello.HelloWorld())
+	t, err := template.ParseFiles("public/index.html")
+	if err != nil {
+		panic(err)
+	}
+	t.Execute(w, nil)
 }
