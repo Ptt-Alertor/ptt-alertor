154f6c443100274a1752f071c403e50385ef65f0
DinoLai <dinos80152@gmail.com>
2017-06-26 03:29:58 +0800

    remove print

--- myutil/config.go
+++ myutil/config.go
@@ -2,12 +2,10 @@ package myutil
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 )
 
 func Config(name string) map[string]string {
-	fmt.Println("Reading Config: " + name)
 	projectRoot := ProjectRootPath()
 	filePath := projectRoot + "/config/" + name + ".json"
 	configJSON, err := ioutil.ReadFile(filePath)
