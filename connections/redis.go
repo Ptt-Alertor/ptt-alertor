329187c42e4f31efa44357b85dea46d838c32c37
DinoLai <dinos80152@gmail.com>
2017-07-04 21:30:07 +0800

    try redigo connection pool, close #76

--- connections/redis.go
+++ connections/redis.go
@@ -1,21 +1,35 @@
 package connections
 
 import (
-	log "github.com/meifamily/logrus"
+	"time"
+
 	"github.com/garyburd/redigo/redis"
+	log "github.com/meifamily/logrus"
 	"github.com/meifamily/ptt-alertor/myutil"
 )
 
 var config map[string]string
+var pool = newPool()
 
 func init() {
 	config = myutil.Config("redis")
 }
 
-func Redis() redis.Conn {
-	conn, err := redis.Dial("tcp", config["host"]+":"+config["port"])
-	if err != nil {
-		log.Fatal(err)
+func newPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 300 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			conn, err := redis.Dial("tcp", config["host"]+":"+config["port"])
+			if err != nil {
+				log.Fatal(err)
+			}
+			return conn, err
+		},
 	}
-	return conn
+}
+
+// Redis get redis connection
+func Redis() redis.Conn {
+	return pool.Get()
 }
