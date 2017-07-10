eb46cc4922e9e3879687ef5a5fab5b8f98500a02
DinoLai <dinos80152@gmail.com>
2017-07-11 00:56:24 +0800

    add pushsum models

--- models/pushsum/pushsum.go
+++ models/pushsum/pushsum.go
@@ -9,6 +9,20 @@ import (
 
 const prefix string = "pushsum:"
 
+var NumTextMap = map[int]string{
+	100:  "爆",
+	-10:  "X1",
+	-20:  "X2",
+	-30:  "X3",
+	-40:  "X4",
+	-50:  "X5",
+	-60:  "X6",
+	-70:  "X7",
+	-80:  "X8",
+	-90:  "X9",
+	-100: "XX",
+}
+
 func List() []string {
 	conn := connections.Redis()
 	defer conn.Close()
@@ -17,6 +31,16 @@ func List() []string {
 		log.WithField("runtime", myutil.BasicRuntimeInfo()).WithError(err).Error()
 	}
 	return boards
+}
+
+func Exist(board string) bool {
+	conn := connections.Redis()
+	defer conn.Close()
+	bl, err := redis.Bool(conn.Do("SISMEMBER", prefix+"boards", board))
+	if err != nil {
+		log.WithField("runtime", myutil.BasicRuntimeInfo()).WithError(err).Error()
+	}
+	return bl
 }
 
 func Add(board string) error {
@@ -37,4 +61,41 @@ func AddSubscriber(board, account string) error {
 		log.WithField("runtime", myutil.BasicRuntimeInfo()).WithError(err).Error()
 	}
 	return err
+}
+
+func RemoveSubscriber(board, account string) error {
+	conn := connections.Redis()
+	defer conn.Close()
+	_, err := conn.Do("SREM", prefix+board+":subs", account)
+	if err != nil {
+		log.WithField("runtime", myutil.BasicRuntimeInfo()).WithError(err).Error()
+	}
+	return err
+}
+
+func ListSubscribers(board string) []string {
+	conn := connections.Redis()
+	defer conn.Close()
+	subs, err := redis.Strings(conn.Do("SMEMBERS", prefix+board+":subs"))
+	if err != nil {
+		log.WithField("runtime", myutil.BasicRuntimeInfo()).WithError(err).Error()
+	}
+	return subs
+}
+
+func DiffList(account, board, kind string, ids ...int) []int {
+	nowKey := prefix + account + ":" + board + ":" + kind + ":now"
+	preKey := prefix + account + ":" + board + ":" + kind + ":pre"
+	conn := connections.Redis()
+	defer conn.Close()
+	conn.Send("MULTI")
+	conn.Send("SADD", redis.Args{}.Add(nowKey).AddFlat(ids)...)
+	conn.Send("SDIFF", nowKey, preKey)
+	conn.Send("RENAME", nowKey, preKey)
+	r, err := redis.Values(conn.Do("EXEC"))
+	ids, err = redis.Ints(r[1], err)
+	if err != nil {
+		log.WithField("runtime", myutil.BasicRuntimeInfo()).WithError(err).Error()
+	}
+	return ids
 }
