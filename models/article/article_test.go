7ba448a6ff7ef007ec753eca9d2fe23f2ba50190
DinoLai <dinos80152@gmail.com>
2021-05-29 22:13:04 +0800

    :white_check_mark: perpare article model test

---
+++ models/article/article_test.go
@@ -0,0 +1,3 @@
+package article
+
+var test = `{"ID":1602349944,"code":"M.1602349944.A.250","Title":"[心得] 我不是耳機，是個工具!Nathaniel Baldwin","Link":"https://www.ptt.cc/bbs/Headphone/M.1602349944.A.250.html","pushList":[{"Tag":"推 ","UserID":"Yazilightar","Content":": 向老前輩致敬XDD","DateTime":"2020-10-11T15:46:00+08:00"}],"lastPushDateTime":"2020-10-11T15:46:00+08:00","board":"Headphone"}`
