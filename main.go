5dcca31c1e6d512e6f6888b1a8b741a9fd047d84
DinoLai <dinos80152@gmail.com>
2021-05-30 00:39:12 +0800

    :triangular_flag_on_post: comment out migrate article

--- main.go
+++ main.go
@@ -161,5 +161,5 @@ func init() {
 	// jobs.NewCacheCleaner().Run()
 	// jobs.NewGenerator().Run()
 	// jobs.NewFetcher().Run()
-	jobs.NewMigrateArticle().Run()
+	// jobs.NewMigrateArticle().Run()
 }
